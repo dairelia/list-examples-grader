d79342652e1d507f6008e2e1baa247bfd091b297
dairelia <114545141+dairelia@users.noreply.github.com>
2022-11-10 12:47:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,3 +5,11 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+if [[ -f List-Examples.java ]]
+then 
+    echo "ListExamples file found"
+else 
+    echo "no file named ListExamples.java"
+fi
+
+cp TestListExamples.java student-submission
