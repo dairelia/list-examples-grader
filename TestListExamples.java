5d8e4fa9b3f798adf98e2b62c6d317c230464a0e
dairelia <114545141+dairelia@users.noreply.github.com>
2022-11-27 22:30:57 -0800

    trial

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,37 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+
+
+class Checks implements StringChecker {
+	public boolean checkString(String s){
+		if(s.length() < 4){
+			return true;
+		}
+
+		else{
+			return false;
+		}
+	}
+}
 
 public class TestListExamples {
-  // Write your grading tests here!
+	@Test(timeout = 500)
+	public void testFilter(){
+		StringChecker sc = new Checks();
+		ArrayList before = new ArrayList<String> ();
+		before.add("hello");
+		before.add("sis");
+		ArrayList after = new ArrayList<String> ();
+		after.add("sis");
+		assertEquals(after, ListExamples.filter(before, sc));
+	}
+
+	@Test(timeout = 500)
+	public void testMerge(){
+		ArrayList list1 = new ArrayList<String>(Arrays.asList("a", "c", "d"));
+		ArrayList list2 = new ArrayList<String>(Arrays.asList("b", "e"));
+		ArrayList finalList = new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e"));
+		assertEquals(finalList, ListExamples.merge(list1, list2));
+	}
 }
--- grade.sh
+++ grade.sh
@@ -1,15 +1,39 @@
 # Create your grading script here
-
+CPATH = "-cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 set -e
 
 rm -rf student-submission
 git clone $1 student-submission
 
-if [[ -f List-Examples.java ]]
+cd student-submission # go into student-sub to look for listexamples
+
+if [[ -f ListExamples.java ]]
 then 
     echo "ListExamples file found"
 else 
     echo "no file named ListExamples.java"
+    exit
 fi
 
+cd .. # go back to copy testlistexamples into studentsubmission
+
 cp TestListExamples.java student-submission
+cp -R lib/ student-submission
+
+# checking if lib is in student-sub
+cd student-submission
+#...
+
+set +e
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> error.txt
+if [[ $? -eq 0 ]]
+then
+	echo "success"
+else
+	cat error.txt
+	exit 1
+fi
+
+
